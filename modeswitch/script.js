09c1f728ce241e744bb4d688d65b36e9000a8856
Leon Berkemeier <leonberkemeier@gmail.com>
2023-10-05 17:03:10 +0200

    update

--- modeswitch/script.js
+++ modeswitch/script.js
@@ -6,10 +6,10 @@ toggle.addEventListener('click', function(){
     if(this.classList.toggle('bi-brightness-high-fill')){
         body.style.background = 'white';
         body.style.color = 'black';
-        body.style.transition = '2s';
+        body.style.transition = '1s';
     }else{
-        body.style.background = 'black';
+        body.style.background = 'rgb(23, 23, 23)';
         body.style.color = 'white';
-        body.style.transition = '2s';
+        body.style.transition = '1s';
     }
 });
